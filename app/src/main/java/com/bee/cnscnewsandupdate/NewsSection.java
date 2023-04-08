ceef3b948290bfbe64c1c8ea0a3a90c606448343
Brylle Justin Heraldo <bryllejheraldo@gmail.com>
2023-04-09 00:57:15 +0800

    Initial commit

---
+++ app/src/main/java/com/bee/cnscnewsandupdate/NewsSection.java
@@ -0,0 +1,47 @@
+package com.bee.cnscnewsandupdate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import android.os.Bundle;
+
+public class NewsSection extends AppCompatActivity {
+
+    private String[] tabs = {"Today's News", "Announcement", "Department News", "Breakthrough   "};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_section);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label1));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label2));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label3));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label4));
+
+        final ViewPager2 viewPager = findViewById(R.id.pager);
+        final PageAdapter adapter = new PageAdapter(getSupportFragmentManager(), getLifecycle());
+        viewPager.setAdapter(adapter);
+
+        adapter.addFragment(new todays_news());
+        adapter.addFragment(new announcement_news());
+        adapter.addFragment(new department_news());
+        adapter.addFragment(new breakthrough_news());
+
+        viewPager.setAdapter(adapter);
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> tab.setText(tabs[position])).attach();
+
+    }
+}
---
+++ app/src/main/java/com/bee/cnscnewsandupdate/loginregister.java
@@ -0,0 +1,85 @@
+package com.bee.cnscnewsandupdate;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class loginregister extends AppCompatActivity {
+
+    TextView create_account_text;
+
+    private FirebaseAuth auth;
+
+    private Button loginButton;
+
+    private EditText loginEmail, loginPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginregister);
+
+        create_account_text = (TextView) findViewById(R.id.register_account_text);
+
+        auth = FirebaseAuth.getInstance();
+        loginEmail = findViewById(R.id.email_login);
+        loginPassword = findViewById(R.id.password_login);
+        loginButton = findViewById(R.id.accountlogin_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = loginEmail.getText().toString();
+                String pass = loginPassword.getText().toString();
+
+                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    if (!pass.isEmpty()) {
+                        auth.signInWithEmailAndPassword(email, pass)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(loginregister.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(loginregister.this, MainActivity.class));
+                                        finish();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(loginregister.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    } else {
+                        loginPassword.setError("Password cannot be empty");
+                    }
+                } else if (email.isEmpty()) {
+                    loginEmail.setError("Email cannot be empty");
+                } else {
+                    loginEmail.setError("Please enter valid email");
+                }
+
+            }
+        });
+
+
+    }
+
+    public void onclickregisteraccount(View view) {
+        Intent create_account_intent = new Intent(this, createaccount.class);
+        finish();
+        startActivity(create_account_intent);
+    }
+}
