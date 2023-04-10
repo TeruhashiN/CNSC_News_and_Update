913a851bf80ab57d25de98ec1bb235da04952df9
Brylle Justin Heraldo <bryllejheraldo@gmail.com>
2023-04-10 11:38:01 +0800

    Adding Administrator Uploading Data

---
+++ app/src/main/java/com/bee/cnscnewsandupdate/Upload_todays_news.java
@@ -0,0 +1,14 @@
+package com.bee.cnscnewsandupdate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Upload_todays_news extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_todays_news);
+    }
+}
---
+++ app/src/main/java/com/bee/cnscnewsandupdate/admin_NewsSection.java
@@ -0,0 +1,59 @@
+package com.bee.cnscnewsandupdate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class admin_NewsSection extends AppCompatActivity {
+
+    FloatingActionButton fab;
+
+    private String[] tabs = {"Today's News", "Announcement", "Department News", "Breakthrough   "};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_news_section);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label1));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label2));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label3));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label4));
+
+        final ViewPager2 viewPager = findViewById(R.id.pager);
+        final PageAdapter adapter = new PageAdapter(getSupportFragmentManager(), getLifecycle());
+        viewPager.setAdapter(adapter);
+
+        adapter.addFragment(new admin_todays_news());
+        adapter.addFragment(new admin_announcement_news());
+        adapter.addFragment(new admin_department_news());
+        adapter.addFragment(new admin_breakthrough_news());
+
+        viewPager.setAdapter(adapter);
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> tab.setText(tabs[position])).attach();
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(admin_NewsSection.this, Upload_todays_news.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
