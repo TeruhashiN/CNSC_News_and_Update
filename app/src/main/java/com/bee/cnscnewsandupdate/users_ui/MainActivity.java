28b289fdfa332cf02dd4ec1a7666cd72f79eb10d
Brylle Justin Heraldo <bryllejheraldo@gmail.com>
2023-04-29 02:20:17 +0800

    New Design using Bottom Navigation

--- app/src/main/java/com/bee/cnscnewsandupdate/users_ui/MainActivity.java
+++ app/src/main/java/com/bee/cnscnewsandupdate/users_ui/MainActivity.java
@@ -1,40 +1,60 @@
 package com.bee.cnscnewsandupdate.users_ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
 
 import com.bee.cnscnewsandupdate.R;
+import com.bee.cnscnewsandupdate.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends AppCompatActivity {
-    ImageButton button_settings;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        button_settings = (ImageButton) findViewById(R.id.setting_button);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+
+                case R.id.homepage:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.searchpage:
+                    replaceFragment(new SearchFragment());
+                    break;
+                case R.id.newspage:
+                    Intent news_intent = new Intent(this, NewsSection.class);
+                    startActivity(news_intent);
+                    break;
+                case R.id.settingspage:
+                    replaceFragment(new SettingsFragment());
+
+                    break;
+
+
+
+            }
+            return true;
+        });
+
     }
 
-    public void onclicksettings(View view) {
-        Intent setting_intent = new Intent(this, Settings.class);
-        startActivity(setting_intent);
-        overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
+    private void replaceFragment(Fragment fragment) {
 
-    }
-    public void onclicknews(View view) {
-        Intent news_intent = new Intent(this, NewsSection.class);
-        startActivity(news_intent);
-        overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
-    }
-    public void onclicksearch(View view) {
-        Intent search_intent = new Intent(this, Settings.class);
-        startActivity(search_intent);
-        overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+
     }
 
 
