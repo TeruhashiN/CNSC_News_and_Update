9ee5bf3b487f09d3144de3048b6fbd6f14719271
Brylle Justin Heraldo <bryllejheraldo@gmail.com>
2023-05-06 19:52:07 +0800

    adding logout button

--- app/src/main/java/com/bee/cnscnewsandupdate/users_ui/MainActivity.java
+++ app/src/main/java/com/bee/cnscnewsandupdate/users_ui/MainActivity.java
@@ -1,13 +1,13 @@
 package com.bee.cnscnewsandupdate.users_ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.content.Intent;
-import android.os.Bundle;
 
 import com.bee.cnscnewsandupdate.R;
 import com.bee.cnscnewsandupdate.databinding.ActivityMainBinding;
--- app/src/main/java/com/bee/cnscnewsandupdate/users_ui/SettingsFragment.java
+++ app/src/main/java/com/bee/cnscnewsandupdate/users_ui/SettingsFragment.java
@@ -10,9 +10,14 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -20,6 +25,7 @@ import android.widget.Toast;
 
 import com.bee.cnscnewsandupdate.R;
 import com.bee.cnscnewsandupdate.login_and_register_system.ReadWriteUserDetails;
+import com.bee.cnscnewsandupdate.login_and_register_system.activity_register;
 import com.bee.cnscnewsandupdate.login_and_register_system.login_system;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -33,6 +39,7 @@ import com.google.firebase.database.ValueEventListener;
 public class SettingsFragment extends Fragment {
 
     private TextView textViewWelcome, textViewFullName, textViewEmail, textViewDoB, textViewGender, textViewMobile;
+    private Button signout_button;
     private ProgressBar progressBar;
     private String fullName, email, doB, gender, mobile;
     private ImageView imageView;
@@ -56,8 +63,11 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        setHasOptionsMenu(true); // enable options menu handling
+        return view;
     }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -78,10 +88,18 @@ public class SettingsFragment extends Fragment {
         textViewMobile = view.findViewById(R.id.textView_show_mobile);
         progressBar = view.findViewById(R.id.progressBar);
 
+        signout_button = view.findViewById(R.id.signout_button);
+        signout_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+
         authProfile = FirebaseAuth.getInstance();
         FirebaseUser firebaseUser = authProfile.getCurrentUser();
 
-        if (firebaseUser == null){
+        if (firebaseUser == null) {
             Toast.makeText(requireContext(), "Something went wrong! User's details are not available at the moment", Toast.LENGTH_LONG).show();
         } else {
             checkIfEmailVerified(firebaseUser);
@@ -91,7 +109,7 @@ public class SettingsFragment extends Fragment {
     }
 
     private void checkIfEmailVerified(FirebaseUser firebaseUser) {
-        if (!firebaseUser.isEmailVerified()){
+        if (!firebaseUser.isEmailVerified()) {
             showAlertDialog();
         }
 
@@ -150,6 +168,16 @@ public class SettingsFragment extends Fragment {
 
             }
         });
-
     }
+
+    private void signOut() {
+        authProfile.signOut();
+        Intent intent = new Intent(requireActivity(), login_system.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Clear the back stack
+        startActivity(intent);
+        requireActivity().finish(); // Close the current activity
+    }
+
+
+
 }
