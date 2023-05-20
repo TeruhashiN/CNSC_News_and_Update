c9355d171e25de0d34581fb351eb0ecba8438a88
Brylle Justin Heraldo <bryllejheraldo@gmail.com>
2023-05-20 16:01:27 +0800

    seperation admin and user

---
+++ app/src/main/java/com/bee/cnscnewsandupdate/Announcement_data/UpdateActivity.java
@@ -0,0 +1,16 @@
+package com.bee.cnscnewsandupdate.Announcement_data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bee.cnscnewsandupdate.R;
+
+public class UpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+    }
+}
